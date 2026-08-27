0d9996c88c50e38003e9cd6c687c051487656245
Liubomyr Dzvinchuk <liubomyr.dzvinchuk-pi2302@kep.nung.edu.ua>
2026-08-27 19:32:37 +0100

    Add features strip and popular post carousel

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function scaleToFit() {
-  const scale = window.innerWidth / 1920;
-  document.body.style.transform = `scale(${scale})`;
-  document.body.style.height = (window.innerHeight / scale) + 'px';
-}
-
-scaleToFit();
-window.addEventListener('resize', scaleToFit);
